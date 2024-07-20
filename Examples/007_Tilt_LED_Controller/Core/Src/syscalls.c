b26432ad943460a9cde470c14d0775520686f9c0
ShreyAcharya <shreyasacharya49@gmail.com>
2024-07-20 15:58:44 +0530

    Updated tilt led controller

--- Examples/007_Tilt_LED_Controller/Core/Src/syscalls.c
+++ Examples/007_Tilt_LED_Controller/Core/Src/syscalls.c
@@ -30,6 +30,27 @@
 #include <sys/time.h>
 #include <sys/times.h>
 
+//Debug Exception and Monitor Control Register base address
+#define DEMCR         *((volatile uint32_t*) 0xE000EDFCU )
+
+/* ITM register addresses */
+#define ITM_STIMULUS_PORT0   *((volatile uint32_t*) 0xE0000000 )
+#define ITM_TRACE_EN           *((volatile uint32_t*) 0xE0000E00 )
+
+void ITM_SendChar(uint8_t ch)
+{
+  //Enable TRCENA
+  DEMCR |= ( 1 << 24);
+
+  //enable stimulus port 0
+  ITM_TRACE_EN |= ( 1 << 0);
+
+  // read FIFO status in bit [0]:
+  while(!(ITM_STIMULUS_PORT0 & 1));
+
+  //Write to ITM stimulus port0
+  ITM_STIMULUS_PORT0 = ch;
+}
 
 /* Variables */
 extern int __io_putchar(int ch) __attribute__((weak));
@@ -84,7 +105,8 @@ __attribute__((weak)) int _write(int file, char *ptr, int len)
 
   for (DataIdx = 0; DataIdx < len; DataIdx++)
   {
-    __io_putchar(*ptr++);
+    //__io_putchar(*ptr++);
+	  ITM_SendChar(*ptr++);
   }
   return len;
 }
